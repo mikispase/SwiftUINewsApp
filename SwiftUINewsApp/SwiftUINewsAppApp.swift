509d5841a1f61c6489ac8dad8ca0f3d4705a03f9
Dimitar Spasovski <dimitar.spasovski@movic.nl>
2021-09-20 21:30:16 +0200

    Initial Commit

---
+++ SwiftUINewsApp/SwiftUINewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINewsAppApp.swift
+//  SwiftUINewsApp
+//
+//  Created by Dimitar Spasovski on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
